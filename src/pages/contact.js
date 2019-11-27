8089b8fba4dab50c07d80d41a7692cff2e89bf77
exshak <sbijabhai@gmail.com>
2019-11-26 21:46:22 -0500

    moved map components

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import SEO from '../components/common/seo'
 import Contact from '../components/contact/Contact'
-import Map from '../components/contact/Map'
 import Layout from '../components/layout/layout'
+import Map from '../components/map/Map'
 
 export default () => (
   <Layout>

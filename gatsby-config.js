5e9e2fa895a20b4ec93ca146d103db534fcd693b
exshak <sbijabhai@gmail.com>
2019-11-27 21:55:25 -0500

    updated config, readme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 })
+
 module.exports = {
   siteMetadata: {
     title: `Cafelina`,
-    description: `Cafelina`,
+    description: `Cafe & Bakery`,
     author: `exshak`,
   },
   plugins: [
@@ -13,14 +14,13 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/assets/images`,
       },
     },
     {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
-        // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
@@ -36,17 +36,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `exshak`,
-        short_name: `exshak`,
+        name: `Cafelina`,
+        short_name: `Cafelina`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/logo.svg`, // This path is relative to the root of the site.
+        icon: `src/assets/icons/logo.svg`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
   ],
 }
